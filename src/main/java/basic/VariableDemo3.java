ac001cf8d0876e34a0458d818440c95d3565d15f
nttd-pengfei.deng <pengfei.deng@nttdata.com>
2020-02-02 12:21:45 +0800

    more

--- src/main/java/basic/VariableDemo3.java
+++ src/main/java/basic/VariableDemo3.java
@@ -17,6 +17,23 @@ public class VariableDemo3 {
 
     System.out.println("div=" + (a / b));
 
+    System.out.println(5 / 2);
+    System.out.println((int) 5.0 / 2);//显示类型转换 ，当我们进行缩窄类型的时候必须显示调用
+    System.out.println(5.0 / 2); //拓宽类型，自动完成，无需显示调用
+
+    float a1 = 1.0f;
+    float a2 = 1.0f;
+
+    System.out.println(a1 + a2);
+    System.out.println(a1 - a2);
+    System.out.println(a1 * a2);
+    System.out.println(a1 / a2);
+
+    //取模操作符获取两数相除的余数
+    System.out.println(5 % 2);
+    System.out.println(5.0 % 2);
+
+
   }
 
 }
