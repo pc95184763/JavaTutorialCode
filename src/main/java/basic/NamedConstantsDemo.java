22b9fb4dd726cfd1e041c9bd6915bbd067792cd9
nttd-pengfei.deng <pengfei.deng@nttdata.com>
2020-02-11 20:44:28 +0800

    reversion java programming base

--- src/main/java/basic/NamedConstantsDemo.java
+++ src/main/java/basic/NamedConstantsDemo.java
@@ -3,9 +3,14 @@ package basic;
 public class NamedConstantsDemo {
 
   public static void main(String[] args) {
-    final double PI = 3.1415926;// 定义一个常量
+    final double PI ;
+    PI = 3.1415926;// 定义一个常量
 
     System.out.println(PI);
+
+    System.out.println(PI *3);
+
+    // PI =1.0;
 
 
 
--- src/main/java/basic/NamingConventionDemo.java
+++ src/main/java/basic/NamingConventionDemo.java
@@ -6,6 +6,10 @@ public class NamingConventionDemo {
     //所有字母必须大写
     final double PI = 3.1415926;// 定义一个常量
 
+    final int MAX_VALUE = Integer.MAX_VALUE;
+
+    double radius;
+
 
     public static void main(String[] args) {
 
